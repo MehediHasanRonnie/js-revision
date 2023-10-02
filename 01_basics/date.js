a1319deb4255a33b8a84aeffd877b23f7ab2f2ff
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2023-10-02 23:22:53 +0600

    commited

---
+++ 01_basics/date.js
@@ -0,0 +1,25 @@
+//Date
+
+let myDate = new Date();
+console.log(myDate.getHours());
+myDate.toLocaleString("default", {
+  weekday: "long",
+});
+
+// console.log(myDate);
+// console.log(myDate.toISOString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+// let newDate = new Date(2023, 0, 1);
+// let newDate = new Date(2023, 0, 1, 5, 6);
+//let newDate = new Date("2014-02-14");
+//console.log(newDate.toLocaleString());
+
+//let myTimeStamps = Date.now();
+// console.log(myTimeStamps);
+// console.log(newDate.getTime());
+
+//console.log(Math.floor(Date.now() / 1000));
---
+++ 01_basics/numsANDmaths.js
@@ -0,0 +1,31 @@
+// const score = 300;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 132.673783;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+//**********************MATHS************************/
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.49));
+// console.log(Math.ceil(5.49));
+// console.log(Math.floor(5.9));
+// console.log(Math.max(5, 9, 8, 4, 23));
+// console.log(Math.min(5, 9, 8, 4, 23));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.random() * (max - min + 1) + min);
